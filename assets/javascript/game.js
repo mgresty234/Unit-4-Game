46feb9d1cb276ecd017ccf09d924d0faf2e5ec96
Matt G <mgresty223@gmail.com>
2018-04-06 18:31:23 -0400

    Unit-4-Game

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -9,7 +9,28 @@ var charFour = "#char4";
 /* Array */
 var characters = [charOne, charTwo, charThree, charFour];
 
+    $(".first").on("click", ".charSection", function() {
+        $(this).addClass("Hero");
+        var chars = $(".charSection");
+        chars.each(function(i, char){
+            if (!char.className.includes("Hero")){
+                $(char).appendTo($(".enemySection"));
+            }
+        });
+    });
+    $(".second").on("click", ".charSection", function() {
+        $(this).addClass("Enemy");
+        var enemy = $(".charSection");
+        enemy.each(function(i, enemy){
+            if (enemy.className.includes("Enemy")){
+                $(enemy).appendTo($(".fourth"));
+            }
+        });
+    });
+    $(".attackBtn").on("click", function() {   
+         
 
+    }
 
 
 

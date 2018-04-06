d121cec2fd4b994b823be07baf09a2e194aac7e7
Matt G <mgresty223@gmail.com>
2018-04-05 21:10:41 -0400

    Unit-4-Game

---
+++ assets/javascript/game.js
@@ -0,0 +1,19 @@
+$(document).ready(function() {
+
+/* Characters */
+var charOne = "#char1";
+var charTwo = "#char2";
+var charThree = "#char3";
+var charFour = "#char4";
+
+/* Array */
+var characters = [charOne, charTwo, charThree, charFour];
+
+
+
+
+
+
+
+});
+
